340b9077fa3deae45c76dc69ba275b09428149be
Che-Wei, Hsu <purpledoor4921@gmail.com>
2019-01-04 16:54:25 +0800

    update README.md

--- README.md
+++ README.md
@@ -21,3 +21,10 @@ plugins=(... assume-role)
 ## Auto input 2fa
 
 You can use [2fa](https://github.com/rsc/2fa) to skip 2fa typing step.
+And if you have a concern with security, that you can chmod 700 for your 2fa.
+
+If using 2fa please export 2fa name
+
+``` zsh
+export _2fa={2fa-name}
+```
--- assume-role.plugin.zsh
+++ assume-role.plugin.zsh
@@ -19,10 +19,12 @@ remove_role() {
 alias vscode=code
 
 assume_role() {
-  # eval $(sudo 2fa {2fa_account}" | assume-role $@ 2>&1 | grep -o "export .*")
-  eval $(command assume-role $@);
+  if [ -z "${_2fa}" ];then
+    eval $(command assume-role $@);
+  else
+    eval $(sudo 2fa ${_2fa} | assume-role $@ 2>&1 | grep -o "export .*")
+  fi
   export ASSUME_DURING=$(date -v +1H +%s); 
 }
 
-# alias assume_role='function(){eval $(command assume-role $@); export ASSUME_DURING=$(date -v +1H +%s); }'
 alias role_detail='function(){unset_aws;env |grep AWS_ACCESS_KEY_ID=;env |grep AWS_SECRET_ACCESS_KEY=;env |grep AWS_SESSION_TOKEN=;env |grep AWS_SECURITY_TOKEN=;}'
