a1b656bffb26a1b00db626a8c3baadc57bc1f6d0
Che-Wei, Hsu <purpledoor4921@gmail.com>
2019-01-04 15:15:48 +0800

    init commit v0.0.1

---
+++ README.md
@@ -0,0 +1,23 @@
+# assume_role plugin
+
+## Method
+
+- assume_role
+- role_detail
+- remove_role
+
+## Install
+
+``` zsh
+git clone https://github.com/weizard/assume-role.git ~/.oh-my-zsh/plugins
+```
+
+## Add Plugin to oh-my-zsh
+
+```zsh
+plugins=(... assume-role)
+```
+
+## Auto input 2fa
+
+You can use [2fa](https://github.com/rsc/2fa) to skip 2fa typing step.
---
+++ assume-role.plugin.zsh
@@ -0,0 +1,28 @@
+unset_aws() {
+  if [ $(date +%s) -ge $ASSUME_DURING ]; then
+    unset ASSUME_DURING;
+    unset AWS_ACCESS_KEY_ID;
+    unset AWS_SECRET_ACCESS_KEY;
+    unset AWS_SESSION_TOKEN;
+    unset AWS_SECURITY_TOKEN;
+  fi  
+}
+
+remove_role() {
+  unset ASSUME_DURING;
+  unset AWS_ACCESS_KEY_ID;
+  unset AWS_SECRET_ACCESS_KEY;
+  unset AWS_SESSION_TOKEN;
+  unset AWS_SECURITY_TOKEN;
+}
+
+alias vscode=code
+
+assume_role() {
+  # echo Password: && eval $(su _2fa -c "~/2fa {2fa_account}" | grep -o '[0-9]\{6\}' | assume-role $@ 2>&1 | grep -o "export .*")
+  eval $(command assume-role $@);
+  export ASSUME_DURING=$(date -v +1H +%s); 
+}
+
+# alias assume_role='function(){eval $(command assume-role $@); export ASSUME_DURING=$(date -v +1H +%s); }'
+alias role_detail='function(){unset_aws;env |grep AWS_ACCESS_KEY_ID=;env |grep AWS_SECRET_ACCESS_KEY=;env |grep AWS_SESSION_TOKEN=;env |grep AWS_SECURITY_TOKEN=;}'
